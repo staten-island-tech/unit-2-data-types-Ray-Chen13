3fb89bd70763d5967bc17c5b0422afad17189d7f
Ray-Chen13 <rayc35@nycstudents.net>
2025-02-06 09:16:20 -0500

    Data Commit

--- app.py
+++ app.py
@@ -7,5 +7,46 @@ values = [1,2.23,5,7,2,30,15]
 for i in values:
     print(i) """
 
-print(values[0])
-print(values[6])
+""" print(values[0])
+print(values[6]) """
+
+"test"
+["t","e","s","t"]
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+""" day_of_week = input("what day is it")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect") """
+
+""" x = "test"
+print(f"hello {x}") """
+
+""" temp = 75
+if temp > 68:
+    print('warm')
+elif temp == 68:
+    print('perfect')
+else:
+    print('cold') """
+
+#Challenge When you use input(), the program stops and asks you to type something in. Once you type something and press Enter, it stores what you typed as a string (a piece of text).
+#Madlibs Project
+
+""" sentence = input("enter a sentence:")
+words = sentence.split()
+count = len(words)
+print(count) """
+
+#Mad Lib Project:
+verb1 = input("")
+verb2 = input("")
+noun = input("")
+number = input("")
+celebrity = input("")
